2d194c83331e91c1dcff475d8a56b98503c9d7bd
Matthias Harter <matthias@brixel.io>
2025-01-08 16:43:40 +0100

    feat: only upload file if it changed

--- src/main/kotlin/de/dhcd/RequestHandler.kt
+++ src/main/kotlin/de/dhcd/RequestHandler.kt
@@ -5,18 +5,26 @@ import com.amazonaws.services.lambda.runtime.RequestStreamHandler
 import com.hierynomus.sshj.userauth.keyprovider.OpenSSHKeyV1KeyFile
 import jakarta.enterprise.context.ApplicationScoped
 import net.schmizz.sshj.SSHClient
+import org.slf4j.LoggerFactory
 import software.amazon.awssdk.core.sync.RequestBody
 import software.amazon.awssdk.services.s3.S3Client
+import software.amazon.awssdk.services.s3.model.HeadObjectRequest
+import software.amazon.awssdk.services.s3.model.NoSuchKeyException
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 import java.io.InputStream
 import java.io.OutputStream
 import java.net.InetAddress
+import java.security.MessageDigest
 
 @ApplicationScoped
 class RequestHandler(
     private val s3Client: S3Client,
     private val configurations: Configurations,
 ) : RequestStreamHandler {
+
+    companion object {
+        val log = LoggerFactory.getLogger(RequestHandler::class.java)
+    }
 
     override fun handleRequest(input: InputStream, output: OutputStream, context: Context) {
         configurations.values.forEach {
@@ -40,11 +48,33 @@ class RequestHandler(
     }
 
     fun upload(config: TargetConfiguration, file: ByteArray) {
-        s3Client.putObject(PutObjectRequest.builder()
-            .bucket(config.s3Bucket)
-            .key(config.s3Key)
-            .build(),
+        val md5Hash = file.toMD5String()
+
+        val headObject = runCatching {
+            s3Client.headObject(
+                HeadObjectRequest.builder()
+                    .bucket(config.s3Bucket)
+                    .key(config.s3Key)
+                    .build()
+            )}
+            .recoverCatching { exception -> if (exception is NoSuchKeyException) null else throw exception }
+            .getOrThrow()
+
+        if(headObject?.eTag() == md5Hash) {
+            log.info("Skipping upload of ${config.s3Key} as the contents are the same (MD5: $md5Hash)")
+            return
+        }
+
+        s3Client.putObject(
+            PutObjectRequest.builder()
+                .bucket(config.s3Bucket)
+                .key(config.s3Key)
+                .build(),
             RequestBody.fromBytes(file)
         )
     }
+}
+
+fun ByteArray.toMD5String(): String {
+    return MessageDigest.getInstance("MD5").digest(this).joinToString("") { "%02x".format(it) }
 }
